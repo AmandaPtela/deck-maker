acb9c50659662b8673d576c2ee6907833c1e67cd
Amanda Portela <mandaptela@gmail.com>
2022-11-09 16:23:01 -0300

    add 'create deck' and 'decks' button

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Header from './Header';
@@ -8,11 +8,13 @@ import { Context } from '../Context/Provider';
 
 function Menu() {
   const page = useLocation();
+  const [criarDeck, setCriarOn] = useState(false);
   const { setDeck } = useContext(Context);
   return (
       <div>
         <Header url={page} title="Menu"/>
         <div  className="menu-geral">
+          { criarDeck ? 
           <div className="form-deck">
             <div id="area-img-menu">
               <img src={ criacao } />
@@ -21,14 +23,42 @@ function Menu() {
               <form>
                 <label id="form-create-deck" htmlFor="input-name-deck"> Dê um nome ao seu baralho
                   <input id="input-name-deck" type="text"/>
-                  <Link to="/criacao">
-                    <button type="button" onClick={ setDeck } id="botao-criar-deck">Iniciar criação</button>
-                  </Link>
+                  <div id="botoes-form-menu">
+                    <Link to="/criacao">
+                      <button type="button" onClick={ setDeck } id="botao-iniciar-criacao">Iniciar criação</button>
+                    </Link>
+                    <button
+                      type="button"
+                      id="botao-criar-baralho-menu"
+                      onClick={ () => setCriarOn(!criarDeck)}
+                    >
+                      Voltar
+                    </button>
+                  </div>
                 </label>
               </form>
             </label>
+          </div> 
+          : 
+            <div id="botoes-menu">
+              <button
+              type="button"
+              id="botao-criar-baralho-menu"
+              onClick={ () => setCriarOn(true)}
+              >
+                Criar Baralho
+              </button>
+              <Link to="/decks">
+              <button
+                type="button"
+                id="ir-decks-menu"
+                >
+                Baralhos
+              </button>
+            </Link>
           </div>
-        </div>
+          }
+        </div> 
       </div>
   );
 }
